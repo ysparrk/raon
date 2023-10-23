9925e9e6cc98ec8e6f1dcc31e359b922cfaa15c6
ysparrk <youngseo0703@gmail.com>
2023-10-23 16:58:33 +0900

    Create: 프로젝트 세팅

---
+++ backend/raon/src/main/java/com/arch/raon/RaonApplication.java
@@ -0,0 +1,13 @@
+package com.arch.raon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaonApplication.class, args);
+	}
+
+}
---
+++ backend/raon/src/test/java/com/arch/raon/RaonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arch.raon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
