cfd92665964e109111de09cf1e042415d4cce15f
고재원 <jason2560@naver.com>
2023-10-24 16:22:29 +0900

    Feat : GrammerRepository 수정

--- backend/raon/src/main/java/com/arch/raon/domain/grammer/repository/GrammerRepository.java
+++ backend/raon/src/main/java/com/arch/raon/domain/grammer/repository/GrammerRepository.java
@@ -1,4 +1,9 @@
 package com.arch.raon.domain.grammer.repository;
 
-public interface GrammerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.arch.raon.domain.grammer.entity.GrammerEntity;
+
+public interface GrammerRepository extends JpaRepository<GrammerEntity, Long> {
+
 }
