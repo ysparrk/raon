a8a496a69dc3342904fb26ae4b3dc612f55123c1
고재원 <jason2560@naver.com>
2023-10-24 16:30:42 +0900

    Feat : GrammerRepository가 JPA를 구현하도록 변경

--- backend/raon/src/main/java/com/arch/raon/domain/grammer/repository/GrammerRepository.java
+++ backend/raon/src/main/java/com/arch/raon/domain/grammer/repository/GrammerRepository.java
@@ -2,8 +2,8 @@ package com.arch.raon.domain.grammer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.arch.raon.domain.grammer.entity.GrammerEntity;
+import com.arch.raon.domain.grammer.entity.GrammerQuizEntity;
 
-public interface GrammerRepository extends JpaRepository<GrammerEntity, Long> {
+public interface GrammerRepository extends JpaRepository<GrammerQuizEntity, Long> {
 
 }
