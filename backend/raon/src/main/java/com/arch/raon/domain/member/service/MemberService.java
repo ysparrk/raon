3abf185848da19693908eb87a70704bec51b1ffa
wooha <jwoohaha@gmail.com>
2023-10-25 15:59:05 +0900

    Feat: 멤버 정보 조회 기능

--- backend/raon/src/main/java/com/arch/raon/domain/member/service/MemberService.java
+++ backend/raon/src/main/java/com/arch/raon/domain/member/service/MemberService.java
@@ -1,4 +1,7 @@
 package com.arch.raon.domain.member.service;
 
+import com.arch.raon.domain.member.dto.response.MemberResDto;
+
 public interface MemberService {
+    MemberResDto findOneMember(Long id);
 }
--- backend/raon/src/main/java/com/arch/raon/domain/member/service/MemberServiceImpl.java
+++ backend/raon/src/main/java/com/arch/raon/domain/member/service/MemberServiceImpl.java
@@ -1,4 +1,22 @@
 package com.arch.raon.domain.member.service;
 
+import com.arch.raon.domain.member.dto.response.MemberResDto;
+import com.arch.raon.domain.member.entity.Member;
+import com.arch.raon.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberServiceImpl {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 조회
+    public MemberResDto findOneMember(Long id){
+        Member findMember = memberRepository.findById(id).get();
+        return MemberResDto.of(findMember);
+    }
 }
