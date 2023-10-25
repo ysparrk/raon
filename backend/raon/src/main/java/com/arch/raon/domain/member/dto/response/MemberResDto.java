237208dfe3f071bdae54d9d6ced082f7709b0b2d
wooha <jwoohaha@gmail.com>
2023-10-25 15:05:11 +0900

    Feat: MemberResDto 정의

--- backend/raon/src/main/java/com/arch/raon/domain/member/dto/response/MemberResDto.java
+++ backend/raon/src/main/java/com/arch/raon/domain/member/dto/response/MemberResDto.java
@@ -1,4 +1,36 @@
 package com.arch.raon.domain.member.dto.response;
 
+import com.arch.raon.domain.member.entity.Member;
+import com.arch.raon.global.util.enums.Gender;
+import com.arch.raon.global.util.enums.School;
+import lombok.Getter;
+
+@Getter
 public class MemberResDto {
+
+    private Long id;
+    private String email;
+    private String nickname;
+    private String profileUrl;
+    private Integer yearOfBirth;
+    private Gender gender;
+    private School school;
+    private Integer mileage;
+    private Boolean isDeleted;
+
+    public static MemberResDto of(Member member) {
+        MemberResDto memberResDto = new MemberResDto();
+        memberResDto.id = member.getId();
+        memberResDto.email = member.getEmail();
+        memberResDto.nickname = member.getNickname();
+        memberResDto.profileUrl = member.getProfileUrl();
+        memberResDto.yearOfBirth = member.getYearOfBirth();
+        memberResDto.gender = member.getGender();
+        memberResDto.school = member.getSchool();
+        memberResDto.mileage = member.getMileage();
+        memberResDto.isDeleted = member.getIsDeleted();
+
+        return memberResDto;
+    }
+
 }
