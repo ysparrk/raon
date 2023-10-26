a1a0b30d35d315a994b7cf4c0838e7b2a5848c7d
junhyuk <slothing96@gmail.com>
2023-10-26 17:01:19 +0900

    Feat : spelling answer

--- frontend/raon/src/components/GameSpelling/Organisms/SpellingAnswer.tsx
+++ frontend/raon/src/components/GameSpelling/Organisms/SpellingAnswer.tsx
@@ -2,30 +2,101 @@ import React from 'react';
 import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
 import { submitState, answerState } from '../../../recoil/Atoms.tsx';
+import ExitButton from '../../Common/Atoms/ExitButton.tsx';
 
-const TestDiv = styled.div`
-  font-family: 'ONE-Mobile-POP';
-  font-size: 200px;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-const TestDiv2 = styled.div`
+const ScoreHeader = styled.h1`
   font-family: 'CookieRun';
-  font-size: 200px;
+  text-align: center;
+  font-size: 70px;
+  width: 100%;
+  margin-top: 50px;
+`;
+
+const Content = styled.div`
+  font-family: 'CookieRun';
+  font-size: 50px;
+  color: #000000;
+`;
+
+const Columns = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const IncorrectAnswer = styled.span`
+  text-decoration: line-through;
+  margin-right: 40px;
+`;
+
+const CorrectAnswer = styled.span`
+  color: red;
 `;
 
 const SpellingAnswer = () => {
   const userAnswers = useRecoilValue(submitState);
   const correctAnswers = useRecoilValue(answerState);
-  console.log(userAnswers, correctAnswers);
+
+  const leftAnswers = userAnswers.slice(0, 5);
+  const rightAnswers = userAnswers.slice(5, 10);
 
   const score = userAnswers.reduce((acc, answer, index) => {
     return answer === correctAnswers[index] ? acc + 1 : acc;
   }, 0);
 
   return (
-    <div>
-      <h1>당신의 점수: {score} / 10</h1>
-    </div>
+    <Container>
+      <ScoreHeader>당신의 점수: {score * 10} 점</ScoreHeader>
+      <Content>
+        <Columns>
+          <Column>
+            {leftAnswers.map((answer, index) => (
+              /* eslint-disable-next-line react/no-array-index-key */
+              <div key={index}>
+                {index + 1}.{' '}
+                {answer === correctAnswers[index] ? (
+                  answer
+                ) : (
+                  <>
+                    <IncorrectAnswer>{answer}</IncorrectAnswer>
+                    <CorrectAnswer>{correctAnswers[index]}</CorrectAnswer>
+                  </>
+                )}
+              </div>
+            ))}
+          </Column>
+          <Column>
+            {rightAnswers.map((answer, index) => (
+              /* eslint-disable-next-line react/no-array-index-key */
+              <div key={index + 5}>
+                {index + 6}.{' '}
+                {answer === correctAnswers[index + 5] ? (
+                  answer
+                ) : (
+                  <>
+                    <IncorrectAnswer>{answer}</IncorrectAnswer>
+                    <CorrectAnswer>{correctAnswers[index + 5]}</CorrectAnswer>
+                  </>
+                )}
+              </div>
+            ))}
+          </Column>
+        </Columns>
+      </Content>
+      <ExitButton to="/" />
+    </Container>
   );
 };
 
