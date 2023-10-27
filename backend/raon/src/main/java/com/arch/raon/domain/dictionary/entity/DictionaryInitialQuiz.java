4115010ccd4ed7de3e554bc70be467087ef7779f
ysparrk <youngseo0703@gmail.com>
2023-10-27 15:33:59 +0900

    Refactor: DictionaryInitialQuiz 이름 변경 및 관련 없는 파일 삭제

--- backend/raon/src/main/java/com/arch/raon/domain/dictionary/entity/DictionaryInitialQuiz.java
+++ backend/raon/src/main/java/com/arch/raon/domain/dictionary/entity/DictionaryInitialQuiz.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
 
 @Entity
-@Table(name = "dictionary_word")
+@Table(name = "dictionary_initial_quiz")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class DictionaryWord {
+@SQLDelete(sql = "UPDATE dictionary_initial_quiz SET deleted_at = now() WHERE dictionary_initial_quiz_id = ?")
+public class DictionaryInitialQuiz {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
