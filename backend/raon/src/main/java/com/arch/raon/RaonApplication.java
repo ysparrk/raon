52de4469514a9a8cf066a92943e66c82620b6d13
김태현 <pooiiiu4100@gmail.com>
2023-11-03 17:06:34 +0900

    Feature : Redis 구조 변경

--- backend/raon/src/main/java/com/arch/raon/RaonApplication.java
+++ backend/raon/src/main/java/com/arch/raon/RaonApplication.java
@@ -2,15 +2,9 @@ package com.arch.raon;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.redis.core.RedisTemplate;
-@EnableCaching
 @SpringBootApplication
-@RequiredArgsConstructor
 @OpenAPIDefinition(
 		servers = {
 				@Server(url = "https://arch-raon.com/api", description = "Default Server url")
@@ -18,12 +12,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 )
 public class RaonApplication {
 
-	private final RedisTemplate redisTemplate;
-
-	@PostConstruct
-	public void setRedisTemplate() {
-		redisTemplate.getConnectionFactory().getConnection().flushAll();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RaonApplication.class, args);
