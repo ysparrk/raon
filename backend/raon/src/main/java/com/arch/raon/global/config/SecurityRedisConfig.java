8b7325d32d2a0fed70b0ae26ae12e4efea101603
김태현 <pooiiiu4100@gmail.com>
2023-11-08 15:29:20 +0900

    Fix : Redis 설정 변경

--- backend/raon/src/main/java/com/arch/raon/global/config/SecurityRedisConfig.java
+++ backend/raon/src/main/java/com/arch/raon/global/config/SecurityRedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,21 +13,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class SecurityRedisConfig {
 
-    @Value("${spring.redis2.host}")
-    private String REDIS2_HOST;
+    @Value("${spring.redis1.host}")
+    private String REDIS1_HOST;
 
-    @Value("${spring.redis2.port}")
-    private int REDIS2_PORT;
+    @Value("${spring.redis1.port}")
+    private int REDIS1_PORT;
 
-    @Value("${spring.redis2.password}")
-    private String REDIS2_PW;
+    @Value("${spring.redis1.password}")
+    private String REDIS1_PW;
 
+    @Primary
     @Bean(name = "SecurityRedisFactory")
     public RedisConnectionFactory securityRedisFactory() {
         LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory();
-        connectionFactory.setHostName(REDIS2_HOST); // 두 번째 Redis 서버 호스트
-        connectionFactory.setPort(REDIS2_PORT); // 두 번째 Redis 서버 포트
-        connectionFactory.setPassword(REDIS2_PW);
+        connectionFactory.setHostName(REDIS1_HOST); // 첫 번째 Redis 서버 호스트
+        connectionFactory.setPort(REDIS1_PORT); // 첫 번째 Redis 서버 포트
+        connectionFactory.setPassword(REDIS1_PW);
         connectionFactory.afterPropertiesSet();
         return connectionFactory;
     }
