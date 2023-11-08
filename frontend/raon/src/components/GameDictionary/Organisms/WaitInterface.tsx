6c589d8ea60f04280438e01f029bb6f833f32108
ysparrk <youngseo0703@gmail.com>
2023-11-08 16:44:58 +0900

    Fix: 웹소켓 중간에 끊어지는 문제 사용자 리스트 주석처리

--- frontend/raon/src/components/GameDictionary/Organisms/WaitInterface.tsx
+++ frontend/raon/src/components/GameDictionary/Organisms/WaitInterface.tsx
@@ -92,11 +92,11 @@ function WaitInterface() {
   const callback: (message: any) => void = (message: any) => {
     if (message.body) {
       const body: any = JSON.parse(message.body);
-      const { users } = body;
-      const gameParticipants = users.map((user: any) => user);
-      setParticipants(gameParticipants);
+      // const { users } = body;
+      // const gameParticipants = users.map((user: any) => user);
+      // setParticipants(gameParticipants);
       console.log(body);
-      console.log(gameParticipants);
+      // console.log(gameParticipants);
     }
   };
 
@@ -139,9 +139,9 @@ function WaitInterface() {
         <RoomCurrentDiv>
           <RoomHeadText>방 코드</RoomHeadText>
           <RoomCodeText>{roomId}</RoomCodeText>
-          {participants.reverse().map((participant, index) => (
+          {/* {participants.reverse().map((participant, index) => (
           <RoomParticipantsText key={index}>{participant}</RoomParticipantsText>
-        ))}
+        ))} */}
         </RoomCurrentDiv>
         <JoinButton
           optionText="초대하기"
@@ -159,9 +159,9 @@ function WaitInterface() {
             console.log("게임 시작 버튼")
             gameStart(stompClient, nickname, roomId)
           }
+          navigate('/game/dictionary-multi-game')
         }}
          />
-        {/* navigate('/game/dictionary-multi-game') */}
         <RoomExitButton onClick={() => {
           if (stompClient) {
             leaveRoom(stompClient, nickname, roomId);
--- frontend/raon/src/components/GameDictionary/Pages/DictionaryMultiGame.tsx
+++ frontend/raon/src/components/GameDictionary/Pages/DictionaryMultiGame.tsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import QuizLetter from '../Organisms/QuizLetter';
-import QuizCrossWord from '../Organisms/QuizCrossWord';
 import TitleBox from '../../Common/Atoms/TitleBox';
-import { getQuiz } from '../../../api/GameDictionaryApi';
-import { dictScoreState } from '../../../recoil/Atoms';
+import ExitButton from '../../Common/Atoms/ExitButtonInRoom';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { useNavigate } from 'react-router-dom';
+
 
 const ContentDiv = styled.div`
   display: flex;
@@ -15,107 +14,15 @@ const ContentDiv = styled.div`
   align-items: center;
 `;
 
-const CountDiv = styled.div`
-  position: fixed;
-  display: flex;
-  top: 5%;
-  right: 5%;
-  font-size: 3.125rem;
-  font-family: 'ONE-Mobile-POP';
-  color: white;
-`;
-function mergeLists(initialList: any[], directionList: any[], totalItems: 10) {
-  const mergedList = [];
-  let initialIndex = 0;
-  let directionIndex = 0;
 
-  for (let i = 0; i < totalItems; i += 1) {
-    if (i % 3 === 2) {
-      // Every third item, add a direction question
-      const directionQuestion = directionList[directionIndex];
-      directionQuestion.type = 'D';
-      mergedList.push(directionQuestion);
-      directionIndex += 1;
-    } else {
-      // Otherwise, add an initial question
-      const initialQuestion = initialList[initialIndex];
-      initialQuestion.type = 'T';
-      mergedList.push(initialQuestion);
-      initialIndex += 1;
-    }
-  }
-
-  return mergedList;
-}
-const DictionaryGame = () => {
-  const [quizList, setQuizList] = useState<any[]>([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const navigate = useNavigate();
-  const setDictScore = useSetRecoilState(dictScoreState);
-
-  useEffect(() => {
-    const fetchQuizList = async () => {
-      try {
-        const response = await getQuiz();
-        const combinedQuiz = await mergeLists(
-          response.data.data.initialQuizList,
-          response.data.data.directionQuizList,
-          10,
-        );
-        console.log(combinedQuiz);
-
-        setQuizList(combinedQuiz);
-      } catch (error) {
-        console.log('catcherror 발생');
-      }
-    };
-    if (quizList.length === 0) {
-      fetchQuizList();
-      setDictScore(0);
-    } else {
-      console.log(quizList);
-    }
-  }, []);
+const DictionaryWaitingRoom = () => {
+  
   return (
     <div>
       <TitleBox>국어사전 놀이</TitleBox>
-      <CountDiv>{currentIndex + 1} / 10</CountDiv>
-      <ContentDiv>
-        {currentIndex < quizList.length &&
-          quizList[currentIndex].type === 'T' && (
-            <QuizLetter
-              word={quizList[currentIndex].word}
-              initial={quizList[currentIndex].initial}
-              meaning={quizList[currentIndex].meaning}
-              nextClick={() => {
-                if (currentIndex < quizList.length - 1) {
-                  setCurrentIndex((prevIndex) => prevIndex + 1);
-                } else {
-                  navigate('/game/dictionary-single-result');
-                }
-              }}
-            />
-          )}
-        {currentIndex < quizList.length &&
-          quizList[currentIndex].type === 'D' && (
-            <QuizCrossWord
-              word={quizList[currentIndex].answer}
-              north_word={quizList[currentIndex].northWord}
-              east_word={quizList[currentIndex].eastWord}
-              west_word={quizList[currentIndex].westWord}
-              south_word={quizList[currentIndex].southWord}
-              nextClick={() => {
-                if (currentIndex < quizList.length - 1) {
-                  setCurrentIndex((prevIndex) => prevIndex + 1);
-                } else {
-                  navigate('/game/dictionary-result');
-                }
-              }}
-            />
-          )}
-      </ContentDiv>
+      <ContentDiv>게임시작</ContentDiv>
     </div>
   );
 };
 
-export default DictionaryGame;
+export default DictionaryWaitingRoom;
