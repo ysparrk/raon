f8d0003a6e9dcb35e9fcb6b18653e11b7d2f9d65
김태현 <pooiiiu4100@gmail.com>
2023-11-09 14:08:45 +0900

    Feature : 국어사전 전국 개인 랭킹 API

--- backend/raon/src/main/java/com/arch/raon/domain/dictionary/service/DictionaryService.java
+++ backend/raon/src/main/java/com/arch/raon/domain/dictionary/service/DictionaryService.java
@@ -1,10 +1,13 @@
 package com.arch.raon.domain.dictionary.service;
 
 import com.arch.raon.domain.dictionary.dto.request.DictionaryScoreReqDTO;
+import com.arch.raon.domain.dictionary.dto.response.DictionaryMyRankResDTO;
 import com.arch.raon.domain.dictionary.dto.response.DictionaryQuizResDTO;
 
 public interface DictionaryService {
     DictionaryQuizResDTO getDictionaryQuizzes();
     void saveDictionaryQuizResult(Long memberId, DictionaryScoreReqDTO dictionaryScoreReqDTO);
+    DictionaryMyRankResDTO getMyRank(Long memberId);
+
 
 }
