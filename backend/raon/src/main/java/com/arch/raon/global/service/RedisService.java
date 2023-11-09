ef3ea8e8c385444218c9ab350d70279c76e6f100
wooha <jwoohaha@gmail.com>
2023-11-09 16:04:35 +0900

    Feat: redis refreshToken key member id로 변경

--- backend/raon/src/main/java/com/arch/raon/global/service/RedisService.java
+++ backend/raon/src/main/java/com/arch/raon/global/service/RedisService.java
@@ -1,6 +1,5 @@
 package com.arch.raon.global.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -32,19 +31,20 @@ public class RedisService {
     }
 
     /** 여기에 리프레쉬 관련 로직 넣으면 좋아용 **/
-    public void setRefreshToken(String accessToken, String refreshToken){
-        // key : accessToken, value : refreshToken
-        securityRedis.opsForValue().set(accessToken, refreshToken);
+    public void setRefreshToken(Long id, String refreshToken){
+        // key : id(member), value : refreshToken
+        securityRedis.opsForValue().set(String.valueOf(id), refreshToken);
         //30일
-        securityRedis.expire(accessToken,30L, TimeUnit.DAYS);
+        securityRedis.expire(String.valueOf(id),30L, TimeUnit.DAYS);
     }
 
-    public String getRefreshToken(String accessToken){
-        return securityRedis.opsForValue().get(accessToken);
+    public String getRefreshToken(Long id){
+        return securityRedis.opsForValue().get(String.valueOf(id));
     }
 
-    public boolean deleteRefreshToken(String accessToken){
-        return securityRedis.delete(accessToken);
+
+    public boolean deleteRefreshToken(Long id){
+        return Boolean.TRUE.equals(securityRedis.delete(String.valueOf(id)));
     }
 
 }
