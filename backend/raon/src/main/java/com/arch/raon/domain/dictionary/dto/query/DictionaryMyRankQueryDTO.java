8139cea5eea610891df853b1bec3d773cb497118
ysparrk <youngseo0703@gmail.com>
2023-11-14 17:18:57 +0900

    Refactor: 국어사전 랭킹 nickname을 ranker로 변경

--- backend/raon/src/main/java/com/arch/raon/domain/dictionary/dto/query/DictionaryMyRankQueryDTO.java
+++ backend/raon/src/main/java/com/arch/raon/domain/dictionary/dto/query/DictionaryMyRankQueryDTO.java
@@ -6,21 +6,21 @@ import org.springframework.data.redis.core.ZSetOperations;
 
 @Getter
 public class DictionaryMyRankQueryDTO {
-    private String nickName;
+    private String ranker;
     private int score;
 
     public DictionaryMyRankQueryDTO() {
     }
 
     @Builder
-    public DictionaryMyRankQueryDTO(String nickName, double score) {
-        this.nickName = nickName;
+    public DictionaryMyRankQueryDTO(String ranker, double score) {
+        this.ranker = ranker;
         this.score = (int) score;
     }
 
     public static DictionaryMyRankQueryDTO convertToDictionaryMyRankQueryDTO(ZSetOperations.TypedTuple typedTuple){
         DictionaryMyRankQueryDTO dictionaryMyRankQueryDTO=new DictionaryMyRankQueryDTO();
-        dictionaryMyRankQueryDTO.nickName= typedTuple.getValue().toString();
+        dictionaryMyRankQueryDTO.ranker = typedTuple.getValue().toString();
         dictionaryMyRankQueryDTO.score=typedTuple.getScore().intValue();
         return dictionaryMyRankQueryDTO;
     }
